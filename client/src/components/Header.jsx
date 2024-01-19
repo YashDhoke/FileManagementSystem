dcfb083d52accfa2ebe2f80ea5ac02ee25ddd64d
YashDhoke <yashdhoke1@gmail.com>
2024-01-20 05:14:45 +0530

    change in header and home page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,10 @@
 import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -12,15 +15,18 @@ export default function Header() {
           </h1>
         </Link>
         <ul className='flex gap-4'>
-          <Link to='/'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>
-              Home
-            </li>
+          <Link to='/' className='text-slate-700'>
+            <li className='hidden sm:inline'>Home</li>
           </Link>
-          <Link to='/sign-in'>
-            <li className=' text-slate-700 hover:underline'> Sign in</li>
-          </Link>
-       
+          {currentUser ? (
+            <Link to='/profile' className='text-slate-700'>
+              <li>{currentUser.username}</li>
+            </Link>
+          ) : (
+            <Link to='/sign-in' className='text-slate-700'>
+              <li>Sign in</li>
+            </Link>
+          )}
         </ul>
       </div>
     </header>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Home() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <div className='my-8'>
       <h1 className='text-7xl'>Welcome to FileDrive</h1>
       <h2 className='text-4xl my-8'>
-         Manage your files with ease!
+        Manage your files with ease!
       </h2>
-          <p className='text-center my-5 text-5xl '>Get Started</p>
+      {currentUser ? (
+        <>
+          <p className='text-center my-5 text-5xl'>
+            View Your Files
+          </p>
+          <Link to={'/profile'}>
+            <span className='text-blue-700 flex justify-center text-2xl'>Go to Profile</span>
+          </Link>
+        </>
+      ) : (
+        <>
+          <p className='text-center my-5 text-5xl'>
+            Get Started
+          </p>
           <Link to={'/sign-up'}>
             <span className='text-blue-700 flex justify-center text-2xl'>Sign Up</span>
           </Link>
+          <span className='text-blue-700 flex justify-center text-2xl'>
+            &nbsp;|&nbsp;
+          </span>
+          <Link to={'/sign-in'}>
+            <span className='text-blue-700 flex justify-center text-2xl'>Sign In</span>
+          </Link>
+        </>
+      )}
     </div>
-
-  )
+  );
 }
